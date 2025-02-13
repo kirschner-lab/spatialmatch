a6ac62d6e8c753459bbdd36843a1375bd4325106
Pariksheet Nanda <pnanda@umich.edu>
2025-02-13 16:32:21 -0500

    ENH: Add MIBI samples figure and tweak PRCC labels

--- R/config.R
+++ R/config.R
@@ -1,6 +1,9 @@
 #' Directory containing the dated model run directories.
 dir_data <- "~/modelruns"
 
+#' Directory containing the MIBI-TOF metadata and cell centroid spreadsheets.
+dir_mibi <- "~/immunology/data"
+
 #' Number of agent-based model time steps per day.  The time step interval is 6
 #' minutes, or 10 steps per hour, and there are 1440 minutes in day.
 ticks_per_day <- 144L
